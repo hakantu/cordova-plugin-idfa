e5abab39ae6b0023824209d3631c7382ce18c75c
H. Turkmen <87485475+hakantu@users.noreply.github.com>
2022-09-02 12:39:54 +0300

    Update IdfaPlugin.java

--- src/android/IdfaPlugin.java
+++ src/android/IdfaPlugin.java
@@ -2,6 +2,8 @@ package by.chemerisuk.cordova.advertising;
 
 import android.content.Context;
 
+import static by.chemerisuk.cordova.support.ExecutionThread.WORKER;
+
 import by.chemerisuk.cordova.support.CordovaMethod;
 import by.chemerisuk.cordova.support.ReflectiveCordovaPlugin;
 
@@ -14,7 +16,7 @@ import org.json.JSONObject;
 public class IdfaPlugin extends ReflectiveCordovaPlugin {
     private static final String TAG = "IdfaPlugin";
 
-    @CordovaMethod(ExecutionThread.WORKER)
+    @CordovaMethod(WORKER)
     protected void getInfo(CallbackContext callbackContext) throws Exception {
         Context context = this.cordova.getActivity().getApplicationContext();
         AdvertisingIdClient.Info info = AdvertisingIdClient.getAdvertisingIdInfo(context);
